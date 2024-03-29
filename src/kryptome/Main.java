88b6aff9dd6628e3431493a2013cc02e774fffd0
YzackZack476 <icc944476@gmail.com>
2024-03-29 15:36:34 -0600

    Script solamente funcional

--- src/kryptome/Main.java
+++ src/kryptome/Main.java
@@ -1,26 +1,29 @@
 package kryptome;
 import GUI.*;
-import java.util.Arrays;
 import java.util.Scanner;
 import javax.swing.BoxLayout;
 import kryptome.Encryptor;
 
 
-public class Kryptome {
+public class Main {
     public static void main(String[] args) throws Exception {
-        Ingress container = new Ingress();
+        Principal container = new Principal();
         container.setVisible(true);
         container.setLocationRelativeTo(null);
         
-        //Encryptor cifradora =  new Encryptor("Clave1234");
-        //Scanner scanner = new Scanner(System.in);
-
+        
+        
+        // ===============================================
         /*
+        Encryptor cifradora =  new Encryptor("Clave1234");
+        Scanner scanner = new Scanner(System.in);
+
+        
         System.out.println("Ingresa mensaje:\n");
         String message = scanner.nextLine();        
         final String msg_cifrado = cifradora.encryptInput(message);
         System.out.println("Escribiste esto: " + msg_cifrado);
-
+        
         System.out.println("Ingresa mensaje cifrado:\n");
         String message = scanner.nextLine();        
         final String msg_decifrado = cifradora.decryptInput(message);
